111366399a10ca3f2f4cbdc052728548f65ac4c6
Rathu Kugan <rathu.kugan@gmail.com>
2015-08-05 16:40:37 -0400

    Cleaned up variable names

--- jobs/taskview.rb
+++ jobs/taskview.rb
@@ -1,21 +1,11 @@
-buzzwords = ['Hire Flight Crew', 'Develop Space Vault', 'Train Flight Crew', 'Venus Consultation', 'Satellite Construct', 'Earth Relations']
-dates = ['64%', '34%', '75%', '90%', '50%', '20%']
+taskListMars = ['Hire Flight Crew', 'Develop Space Vault', 'Train Flight Crew', 'Venus Consultation', 'Satellite Construct', 'Earth Relations']
+progress = ['64%', '34%', '75%', '90%', '50%', '20%']
 
-=begin
-buzzword_counts = Hash.new({ value: 0 })
+task_list = Hash.new
 
-SCHEDULER.every '2s' do
-  random_buzzword = buzzwords.sample
-  buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
-
-  send_event('taskview', { items: buzzword_counts.values })
-end
-=end
-buzzword_counts = Hash.new
-
-buzzwords.each_with_index do |y, j|
-  buzzword_counts[y] = { label: y, value: dates[j]}
-  send_event('taskview', { items: buzzword_counts.values })
+taskListMars.each_with_index do |task, index|
+  task_list[task] = { label: task, value: progress[index]}
+  send_event('taskview', { items: task_list.values })
 end
 
 
@@ -25,8 +15,7 @@ dates = ['2015/08/16', '2015/11/13', '2015/12/31', '2015/11/31']
 
 docs_counts = Hash.new
 
-docs.each_with_index do |x, i|
-  #docs_counts[x] = { label: x, value: (docs_counts[x][:value] + 1) % 10 }
-  docs_counts[x] = { label: x, value: dates[i]}
+docs.each_with_index do |doc, index|
+  docs_counts[doc] = { label: doc, value: dates[index]}
   send_event('docreviews', { items: docs_counts.values })
 end
