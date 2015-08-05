ec55dd8e905447db68d3b92762c8c632429d3c3b
Rathu Kugan <rathu.kugan@gmail.com>
2015-08-05 13:24:51 -0400

    Fixed signoffs and changed colours

--- jobs/taskview.rb
+++ jobs/taskview.rb
@@ -11,9 +11,16 @@ end
 docs = ['VenusPortfolio.docx - Review - Bailey', 'RocketBudget.docx - Signoff - Rath', 'VaultCodes.docx - Signoff - Adam', 'RocketLogs.txt - Signoff - Colin']
 docs_counts = Hash.new({ value: 0 })
 
+=begin
 SCHEDULER.every '30s' do
   random_doc = docs.sample
   docs_counts[random_doc] = { label: random_doc, value: (docs_counts[random_doc][:value] + 1) % 10 }
 
+  send_event('docreviews', { items: docs_counts.values })
+end
+=end
+
+docs.each do |x|
+  docs_counts[x] = { label: x, value: (docs_counts[x][:value] + 1) % 10 }
   send_event('docreviews', { items: docs_counts.values })
 end
